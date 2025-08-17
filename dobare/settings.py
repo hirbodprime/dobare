b336aca1f5403e9550310a17cd00eba14fa514e4
hirbodprime <hirbodprime@gmail.com>
2025-08-17 15:17:30 +0330

    sdfg

--- dobare/settings.py
+++ dobare/settings.py
@@ -120,7 +120,7 @@ STATIC_ROOT = '../dobare/static/'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../dobare/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
